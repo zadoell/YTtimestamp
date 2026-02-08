7d238743244045fc5cf5fdb9389a43d2ca36eecf
Zaryab Hasan <zaryab@Zaryabs-MacBook-Pro.local>
2026-02-08 19:49:31 +0000

    Added 'SuttlebangSuttebang' in App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Helmet>
         <meta
           name="description"
-          content="TubeStamp is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience. Suttlebang"
+          content="TubeStamp is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience. SuttlebangSuttebang"
         />
         <meta
           name="keywords"
